dc16c296b2d24fddf404b28f7deac73d9678ef19
LuizAuGFer <luiz.augusto9922@outlook.com>
2022-12-13 20:47:37 -0300

    backend created

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        
+        $fields = $request;
+
+        $email_check = User::where('email', $request->email)->first();
+        if($email_check)
+        {
+            return response()->json(
+            [
+                'success'   => false, 
+                'title'     => 'Erro no cadastro de usuário', 
+                'message'   => 'Já existe um usuário cadastrado com esse e-mail!'
+            ])->setStatusCode(401);
+        }
+      
+        $user = User::create([
+            'name'      => $fields['name'],
+            'email'     => $fields['email'],
+            'password'  => bcrypt($fields['password']),
+        ]);
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+        
+        
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response()->json(
+            [
+                'success'   => true, 
+                'title'     => 'Usuário cadastrado!', 
+                'message'   => 'Usuário cadastrado com sucesso!',
+                'response'  => $response
+            ])->setStatusCode(201);
+    }
+
+    public function logout(Request $request)
+    {
+        auth()->user()->tokens()->delete();
+
+        return [
+            'message' => 'Logged Out'
+        ];
+    }
+
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        // Check email
+        $user = User::where('email', $fields['email'])->first();
+
+        // Check password
+        if(!$user || !Hash::check($fields['password'], $user->password)) {
+            return response([
+                'message' => 'Bad creds'
+            ], 401);
+        }
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::post('/test', function()
+{
+    return response(201);
+});
+// Public routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Protected routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/products', function(){
+        return 'products';
+    });
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+
